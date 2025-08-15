277cf010141bf2dea5ac57ca87e27cfc10c54aef
Letícia Luna <leticialunadb@gmail.com>
2025-08-15 17:51:14 -0300

    servidor com html básico

--- node-do-zero/server.js
+++ node-do-zero/server.js
@@ -1,13 +1,61 @@
-import { createServer } from 'node:http'
+//sem utilizar framework
 
-const server = createServer((request, response) => {
-    console.log('oi')
+// import { createServer } from 'node:http'
 
-    response.write('que pena que deu errado!!')
+// const server = createServer((request, response) => {
+//     console.log('oi')
 
-    return response.end()
+//     response.write('que pena que deu errado!!')
+
+//     return response.end()
     
+// })
+
+// server.listen(3333)
+
+
+//com fastify
+import { readFileSync } from 'fs'
+
+import { join } from 'path'
+
+import { fastify } from "fastify"
+
+const server = fastify()
+
+server.get('/', () => {
+    return 'Hello world'
 })
 
-server.listen(3333)
+server.get('/atual', () => {
+    return 'Hello pessoa x'
+})
 
+server.get('/html', (request, response) => {
+    response.type('text/html')
+    response.send(`
+        <html>
+            <head>
+                <title>leticias page;p</title>
+            </head>
+            <body>
+                <h1>Deu tudo certo! :) </h1>
+            </body>
+        </html>
+    `)   
+
+})
+
+
+const cardsHtml = readFileSync(join(process.cwd(), '../estudosFrontEnd/playingCards/cards.html'), 'utf-8')
+
+server.get('/html2', (request, response) => {
+    response.type('text/html')
+    response.send(cardsHtml)   
+
+})
+
+
+server.listen({
+    port: 3333,
+})
