251ad07bccfd91194dccca7bd4e230c8d3021f11
Letícia Luna <leticialunadb@gmail.com>
2025-08-21 17:47:42 -0300

    estudando o uso do express

--- backEnd/boilerplate-express/myApp.js
+++ backEnd/boilerplate-express/myApp.js
@@ -1,5 +1,29 @@
+require('dotenv').config();
+
 let express = require('express');
 let app = express();
+
+
+absolutePath = __dirname + '/views/index.html'
+
+
+app.get('/', (req, res) => {
+    res.sendFile(absolutePath);
+})
+
+app.use('/public', express.static(__dirname + '/public'));
+
+
+app.get('/json', (req, res) => {
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        res.json({ "message": "HELLO JSON" });
+  } else {
+        res.json({ "message": "Hello json" });
+  }
+})
+
+
+console.log("Hello World");
 
 
 
